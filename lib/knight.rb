a0a5984c07caea212d8694a92b43f6d139680022
Maefire <masked_phoenix@yahoo.com>
2023-04-20 03:24:43 -0500

    Define basic needs in class
    Made the first version of the initialization. It will take in an array
    of two numbers, assign it to the knight's position (starting pos).
    This will be used to determine possible moves within board constraints.

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,2 +1,19 @@
 class Knight
+  KNIGHT_MOVE = [
+    [2, 1], [1, 2], [-2, -1], [-1, -2],
+    [-2, 1], [1, -2], [-1, 2], [2, -1]
+  ].freeze
+
+  attr_accessor :position, :children
+
+  def initialize(position)
+    self.position = position
+    self.children = []
+  end
+
+  # iterate over KNIGHT_MOVE. position of knight is [0]
+  def valid_moves
+    KNIGHT_MOVE.each do |ea|
+    end
+  end
 end
