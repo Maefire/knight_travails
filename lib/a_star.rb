07009b5a411abd2bbcebe655b4f88c3819d3922c
Maefire <masked_phoenix@yahoo.com>
2023-04-21 11:59:22 -0500

    Add variables and methods for A* algorithm

--- lib/a_star.rb
+++ lib/a_star.rb
@@ -1,8 +1,78 @@
 require_relative "./knight"
 
 class AStar
+  attr_accessor :start, :goal, :open_list, :closed_list
+
+  # this takes a starting array position [0,0] and the goal [0,0]
+  # extract start and goal. They are coordinates, but not part of the algorithm directly?
+  def initialize
+    @start = start
+    @goal = goal
+    @open_list = []
+    @closed_list = []
+  end
+
+  # Finds the shortest path from start to goal using A* algorithm
+  def knight_moves(start, goal)
+    self.start = start
+    self.goal = goal
+    # creates a knight node at the start array position.
+    starting_node = new_node(start)
+
+    open_list << starting_node
+
+    until open_list.empty?
+      current_node = open_list.min_by { |node| node.dist_sum }
+      open_list.delete(current_node)
+      closed_list << current_node
+
+      return construct_path(current_node) if current_node.position == goal
+
+      valid_moves = new_node(current_node.position).valid_moves
+
+      valid_moves.each do |move|
+        next if closed_list.any? { |node| node.position == move }
+
+        start_dist = current_node.dist_from_start + 1
+        goal_dist = distance_from_end(move)
+        total_dist = start_dist + goal_dist
+
+        existing_node = open_list.find { |node| node.position == move }
+        if existing_node
+          if start_dist < existing_node.dist_from_start
+            existing_node.parent = current_node
+            existing_node.dist_from_start = start_dist
+            existing_node.dist_sum = total_dist
+          end
+        else
+          node = new_node(move)
+          node.parent = current_node
+          node.dist_from_start = start_dist
+          node.dist_from_goal = goal_dist
+          node.dist_sum = total_dist
+          open_list << node
+        end
+      end
+    end
+
+    nil  # no path found
+  end
+
+  private
+
   # this takes a valid array for position and goal [0, 0] .. [7, 7] and returns the absolute value. In this case the "Manhattan distance" is used.
-  def calculate_distance(position, goal)
+  def distance_from_end(position)
     (position[0] - goal[0]).abs + (position[1] - goal[1]).abs
+  end
+
+  # Constructs the shortest path from the goal node to the start node recursively. node.position array adds next array onto the list, until no more parents found.
+  def construct_path(node)
+    return node.position if node.parent.nil?
+    [node.position] + construct_path(node.parent)
+  end
+
+  # Creates a new node with the given position
+  def new_node(position)
+    Knight.new(position)
   end
 end
--- lib/knight.rb
+++ lib/knight.rb
@@ -1,17 +1,21 @@
 class Knight
   KNIGHT_MOVE = [
-    [2, 1], [1, 2], [-2, -1], [-1, -2],
-    [-2, 1], [1, -2], [-1, 2], [2, -1]
+    [1, 2], [2, 1], [-1, -2], [-2, -1],
+    [-1, 2], [2, -1], [-2, 1], [1, -2]
   ].freeze
 
-  attr_accessor :position
-  def initialize(position)
-    self.position = position
+  attr_accessor :position, :parent, :dist_from_start, :dist_from_goal, :dist_sum
+  def initialize(position, parent = nil)
+    @position = position
+    @parent = parent
+    @dist_from_start = 0
+    @dist_from_goal = 0
+    @dist_sum = 0
   end
 
   # iterate over KNIGHT_MOVE. position of knight is [4, 3]
   # => [[6, 4], [5, 5], [2, 2], [3, 1], [2, 4], [5, 1], [3, 5], [6, 2]]
-  def valid_moves(position, result = [])
+  def valid_moves(result = [])
     KNIGHT_MOVE.each do |move|
       x = position[0] + move[0]
       y = position[1] + move[1]
